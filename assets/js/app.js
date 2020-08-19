44db83d6c547f400138c62b9721cd3fd02bd350e
Fernanda <cacerespratfernanda@gmail.com>
2020-08-18 21:04:06 -0400

    Almacenando Tweets en Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,12 +33,35 @@ function agregarTweet(e) {
   li.appendChild(botonBorrar);
   // Añade el tweet a la lista
   listaTweets.appendChild(li);
+  // Añadir a Local Storage
+  agregarTweetLocalStorage(tweet);
 }
-
+// Elimina el tweet del DOM
 function borrarTweet(e) {
   e.preventDefault();
   if (e.target.className === 'borrar-tweet') {
     console.log(e.target.parentElement.remove());
     alert('Tweet Eliminado');
   }
+}
+
+// Agregar tweet a Local Storage
+function agregarTweetLocalStorage(tweet) {
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  //añadir el nuevo tweet
+  tweets.push(tweet);
+  // Convertir de string a arreglo para local storage
+  localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+function obtenerTweetsLocalStorage() {
+  let tweets;
+  // Revisamos los valores de local storage
+  if (localStorage.getItem('tweets') === null) {
+    tweets = [];
+  } else {
+    tweets = JSON.parse(localStorage.getItem('tweets'));
+  }
+  return tweets;
 }
